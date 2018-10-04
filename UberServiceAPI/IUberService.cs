0bf1784e4aacf5b4b661c3d910b4f57d576484d3
Anbarasu Angappan <kanbuarasu27@gmail.com>
2018-10-04 12:55:27 +0530

    Add project files.

---
+++ UberServiceAPI/IUberService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace UberServiceAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUberService" in both code and config file together.
+    [ServiceContract]
+    public interface IUberService
+    {
+        [OperationContract]
+        DataTable GetHousdetails(string _houseID, string _apiKey);
+
+        [OperationContract]
+        DataTable GetHouseDetails();
+    }
+}
